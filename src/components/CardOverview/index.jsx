cf60b69bfe0ed20100518ae45097792e1f688208
Lucas Wendell <lucaswhendellf@gmail.com>
2023-01-21 15:58:09 -0300

    creating components

---
+++ src/components/CardOverview/index.jsx
@@ -0,0 +1,5 @@
+import './style.css';
+
+export const CardOverview = () => {
+	return <div>index</div>;
+};
---
+++ src/components/Container/index.jsx
@@ -0,0 +1,5 @@
+import './style.css';
+
+export const Container = () => {
+	return <div className="container">Container</div>;
+};
