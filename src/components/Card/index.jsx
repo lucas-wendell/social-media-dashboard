39de08763aa69a1a3151bb2de798f6e8ed082738
Lucas Wendell <lucaswhendellf@gmail.com>
2023-01-23 17:44:46 -0300

    adding logic in card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,21 +1,45 @@
 import './style.css';
-import facebookLogo from '../../images/icon-facebook.svg';
-import arrowUp from '../../images/icon-up.svg';
 
-export const Card = () => {
+import arrowUp from '../../images/icon-up.svg';
+import arrowDown from '../../images/icon-down.svg';
+import { useCallback, useEffect, useRef } from 'react';
+
+export const Card = ({
+	logo,
+	followersNumber,
+	losingFollowers,
+	followersToday,
+	username,
+}) => {
+	const imgRef = useRef(null);
+
+	const handleImg = useCallback(() => {
+		imgRef.current.setAttribute('src', losingFollowers ? arrowDown : arrowUp);
+	}, [losingFollowers]);
+
+	useEffect(() => {
+		handleImg();
+	}, [handleImg]);
+
 	return (
 		<div className="card">
 			<p className="userName">
-				<img src={facebookLogo} alt="icon-facebook" /> <span>@nathanf</span>
+				<img src={logo} alt="social-media-icon" /> <span>{username}</span>
 			</p>
 			<div className="followersDiv">
-				<h1>1987</h1>
+				<h1>{followersNumber}</h1>
 				<p>followers</p>
 			</div>
 			<div className="todayStatus">
-				<img src={arrowUp} alt="arrow-up" />
-				<p>
-					<span>12</span>Today
+				<img ref={imgRef} alt="arrow-icon" />
+				<p
+					style={{
+						color: losingFollowers
+							? 'hsl(356, 69%, 56%)'
+							: 'hsl(163, 72%, 41%)',
+					}}
+				>
+					<span>{followersToday}</span>Today
 				</p>
 			</div>
 		</div>
--- src/components/CardContainer/index.jsx
+++ src/components/CardContainer/index.jsx
@@ -1,16 +1,64 @@
+import { useState } from 'react';
 import { Card } from '../Card';
 import { Header } from '../Header';
 import './style.css';
+import facebookLogo from '../../images/icon-facebook.svg';
+import twitterLogo from '../../images/icon-twitter.svg';
+import instagramLogo from '../../images/icon-instagram.svg';
+import youtubeLogo from '../../images/icon-youtube.svg';
 
 export const CardContainer = () => {
+	const [state] = useState([
+		{
+			name: 'facebook',
+			username: '@nathanf',
+			logo: facebookLogo,
+			followersNumber: 1987,
+			followersToday: 12,
+			losingFollowers: false,
+		},
+		{
+			name: 'twitter',
+			username: '@nathanf',
+			logo: twitterLogo,
+			followersNumber: 1044,
+			followersToday: 99,
+			losingFollowers: false,
+		},
+		{
+			name: 'instagram',
+			username: '@realnathanf',
+			logo: instagramLogo,
+			followersNumber: '11k',
+			followersToday: 1099,
+			losingFollowers: false,
+		},
+		{
+			name: 'youtube',
+			username: 'Nathan F.',
+			logo: youtubeLogo,
+			followersNumber: 8239,
+			followersToday: 144,
+			losingFollowers: true,
+		},
+	]);
+
 	return (
 		<div className="cardContaier">
 			<Header />
 			<div className="cards">
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+				{state.map(socialMedia => {
+					return (
+						<Card
+							key={socialMedia.name}
+							logo={socialMedia.logo}
+							followersNumber={socialMedia.followersNumber}
+							followersToday={socialMedia.followersToday}
+							losingFollowers={socialMedia.losingFollowers}
+							username={socialMedia.username}
+						/>
+					);
+				})}
 			</div>
 		</div>
 	);
